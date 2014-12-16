73746bdd1f9dcb41f4174073b90e7eb5564e31d7
Martin Karrer <martin@bmalum.com>
2014-12-16 08:38:04 +0100

    added Link and CodeLayout to Impressum

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,12 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get impressum" do
+ 
+ test "should get impressum" do
     get :impressum
     assert_response :success
   end
+ 
  test "should get home" do
     get :home
     assert_response :success
   end
+
 end

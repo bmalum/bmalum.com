b0db79c506d1b78695024d57c48c60ce5ed2d0d7
Martin Karrer <support@bmalum.com>
2014-12-29 11:22:04 +0100

    Tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def new
   	@user = User.new
   end
@@ -10,5 +10,10 @@ class UserController < ApplicationController
   	else
   		render "new"
   	end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User
   include Mongoid::Document
+
+  attr_accessor :password
+
   field :email, type: String
   field :password_hash, type: String
   field :password_salt, type: String
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
 end

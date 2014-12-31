47d6b5aebfed51044898272b7933d7711da56985
Martin Karrer <support@bmalum.com>
2014-12-31 14:41:39 +0100

    Added some Auth Basics TOFO some Roles

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,16 @@ class SessionsController < ApplicationController
   def create
   	user = User.authenticate(params[:email], params[:password])
   	if user 
-  	session[:user_id] = user.id
-  	redirect_to  root_url, :notice  => "Logged in!"
+  	session[:user_id] = user._id.to_s
+  	redirect_to  root_url, :notice  => "Logged in! #{session[:user_id]}"
   	else
   		flash.now.alert = "Invalid email or password"
   		render "new"
   	end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to root_url, :notice => "Logged out!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   get "sign_up" => "users#new", :as => "sign_up"
-
+  
   get "log_in" => "sessions#new", :as => "log_in"
+
+  get "log_out" => "sessions#destroy", :as => "log_out"
 
   root :to => "users#new"
 

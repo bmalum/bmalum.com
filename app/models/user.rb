8ae087334b09af680baebeba86bcb9684c4399df
Martin Karrer <support@bmalum.com>
2015-01-07 16:46:08 +0100

    Inlining Code (no Changes)

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ def self.search(search)
   end
 end
 
-  def self.authenticate(email, password)
+def self.authenticate(email, password)
   	user = find_by(email: email)
   	if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
   		user

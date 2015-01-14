487cb806c9e9298bf6958a991e0c4492e2ba8f34
Martin Karrer <support@bmalum.com>
2015-01-14 20:46:00 +0100

    TidyUp Code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,46 +6,46 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :is_user?
 
-  private 
+  private
   def current_user
-  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  protected 
-   
-  def logged_in? 
-    unless session[:user_id] 
-      flash[:notice] = "You need to log in first." 
-      redirect_to log_in_path 
-      return false 
-    else 
-      return true 
-    end 
-  end
-
- def is_admin?
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    if @current_user.blank?
-      flash[:notice] = "You need to be logged in" 
+  end
+
+  protected
+
+  def logged_in?
+    unless session[:user_id]
+      flash[:notice] = "You need to log in first."
+      redirect_to log_in_path
       return false
     else
-    unless current_user.role.include? "admin"
-      flash[:notice] = "You need to be Admin" 
-      return false 
-    else 
-      return true 
+      return true
     end
   end
-end
+
+  def is_admin?
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if @current_user.blank?
+      flash[:notice] = "You need to be logged in"
+      return false
+    else
+      unless current_user.role.include? "admin"
+        flash[:notice] = "You need to be Admin"
+        return false
+      else
+        return true
+      end
+    end
+  end
 
   def is_user?(role)
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
     return false if @current_user.blank?
     unless current_user.role.include? role
-      flash[:notice] = "You need to be Admin" 
-      return false 
-    else 
-      return true 
+      flash[:notice] = "You need to be Admin"
+      return false
+    else
+      return true
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
   resources :articles do
-    resources :comments 
+    resources :comments
   end
 
   get 'sessions/new'
 
   get "sign_up" => "users#new", :as => "sign_up"
-  
+
   get "log_in" => "sessions#new", :as => "log_in"
 
   get "log_out" => "sessions#destroy", :as => "log_out"

60ae3c734e863e0ce4b15df4c71ac815a33b1423
Martin Karrer <support@bmalum.com>
2015-01-14 10:45:02 +0100

    Registered Helper Method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :is_user?
 
   private 
   def current_user

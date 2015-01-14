67474e1105b4cf66f9d3b1dbc59ab86b527bc045
Martin Karrer <support@bmalum.com>
2015-01-14 20:40:14 +0100

    Added Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,14 @@
 class Article
   include Mongoid::Document
+  include Mongoid::Attributes::Dynamic
+
   field :name, type: String
   field :content, type: String
   field :tags, type: String
   field :category, type: String
-  field :date, type: DateTime
+  field :date, :type => Date
   field :language, type: String
   field :public, type: Mongoid::Boolean
+
+  embeds_many :comments
 end
